aad4039a3a7d953156d6465a0508ba19562abe6a
c3kimball <c3kimball@student.bridgew.edu>
2022-10-17 13:08:53 -0400

    main now creates an empty sql database for the meteorites.

--- database_functions.py
+++ database_functions.py
@@ -7,14 +7,49 @@ import json
 
 
 def get_json_data(website):
-    """Returns a json object from the website link provided.
+    """Returns a json object from the url provided.
      If the request is unsuccessful the program will stop."""
 
     try:
         response_obj = requests.get(website)
         json_obj = response_obj.json()
         return json_obj
-    except requests.exceptions.RequestException as e:
-        print(f"Error in getting getting data: {e}")
+    except requests.exceptions.RequestException as requests_error:
+        print(f"Error in getting getting data: {requests_error}")
         raise SystemExit()
+
+
+def create_database(json_obj):
+    """Creates an SQL database from json_obj."""
+
+    db_connection = None  # Just a failsafe in case the try block is never reached
+
+    try:
+        db_name = 'all_meteorites.db'
+        db_connection = sqlite3.Connection(db_name)
+        db_cursor = db_connection.cursor()
+
+        db_cursor.execute('''CREATE TABLE IF NOT EXISTS meteorite_data_all(
+                        name TEXT,
+                        id INTEGER,
+                        nametype TEXT,
+                        recclass TEXT,
+                        mass TEXT,
+                        fall TEXT,
+                        year TEXT,
+                        reclat TEXT,
+                        reclong TEXT,
+                        geolocation TEXT,
+                        states TEXT,
+                        counties TEXT);''')
+
+        db_cursor.execute('''DELETE FROM meteorite_data_all''')
+
+    except sqlite3.Error as db_error:
+        print(f"A database error has occured: {db_error}")
+        return None
+    finally:
+        if db_connection:
+            db_connection.close()
+        print("Job's done.")
 
--- main.py
+++ main.py
@@ -9,9 +9,13 @@ import database_functions
 
 
 def main():
-    """Gathers a list of meteorites on Earth from NASA's website and filters them into tables base on where they landed."""
+    """Creates a list of meteorites on Earth from NASA's website and filters them into tables based on where they landed."""
+
     all_meteors = database_functions.get_json_data("https://data.nasa.gov/resource/gh4g-9sfh.json")
-    print(all_meteors)
+
+    database_functions.create_database(all_meteors)
+
+    #print(all_meteors)
 
 if __name__ == '__main__':
     main()
